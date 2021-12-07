738cacd87f31c53d4318307684d7f28c771a2d9e
Salma Cruz Sánchez <salmasanchezscs@outlook.com>
2021-12-07 14:54:16 -0600

    Templates
    Archivos para el menú y el footer del sistema operacional y del data warehouse

---
+++ templates/footer.php
@@ -0,0 +1,16 @@
+        <footer class="bg-white sticky-footer">
+        <div class="container my-auto">
+            <div class="text-center my-auto copyright"><span>Pan la migaja</span></div>
+        </div>
+        </footer>
+    </div>
+        <a class="border rounded d-inline scroll-to-top" href="#page-top"><i class="fas fa-angle-up"></i></a>
+</div>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/js/chart.min.js"></script>
+<script src="assets/js/bs-init.js"></script>
+<script src="assets/js/theme.js"></script>
+<script src="assets/js/script.js"></script>
+</body>
+
+</html>
---
+++ templates/menu.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Panadería Pan la Migaja</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="js/jquery-3.6.0.js"></script>
+</head>
+
+<body id="page-top">
+    <div id="wrapper" class="wrapper">
+        <nav class="sidebar navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary  p-0">
+            <div class="container-fluid d-flex flex-column p-0">
+                <a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="principal.php">
+                    <div class="sidebar-brand-icon"><i class="fas fa-store"></i></div>
+                    <div class="sidebar-brand-text mx-3"><span>Bread<br>System</span></div>
+                </a>
+
+                <ul class="nav flex-column sidebar-divider my-0" id="nav_accordion">
+                    <li class="nav-item has-submenu">
+                        <a class="nav-link" href="#"> Sistema operacional </a>
+                        <ul class="submenu collapse">
+                            <li class="nav-item"><a class="nav-link" href="empleados.php"><i class="fas fa-user fa-2x text-gray-300"></i><span>Empleados</span></a></li>
+                            <li class="nav-item"><a class="nav-link" href="materia_prima.php"><i class="fas fa-table fa-2x text-gray-300"></i><span>Materia Prima</span></a></li>
+                            <li class="nav-item"><a class="nav-link" href="produccion.php"><i class="fas fa-truck-moving fa-2x text-gray-300"></i><span>Produccion</span></a></li>
+                            <li class="nav-item"><a class="nav-link" href="punto_venta.php"><i class="fas fa-search-location fa-2x text-gray-300"></i><span>Puntos de Venta</span></a></li>
+                            <li class="nav-item"><a class="nav-link" href="recetario.php"><i class="fas fa-clipboard-list fa-2x text-gray-300"></i><span>Recetario</span></a></li>
+                            <li class="nav-item"><a class="nav-link" href="ventas.php"><i class="fas fa-dollar-sign fa-2x text-gray-300"></i><span>Ventas</span></a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item has-submenu">
+                        <a class="nav-link" href="#"> Rendimiento </a>
+                        <ul class="submenu collapse">
+                            <li><a class="nav-link" href="tablero.php"><i class="fas fa-table fa-2x text-gray-300"></i>Tablero de control </a></li>
+                            <li><a class="nav-link" href="historico.php"><i class="fas fa-table fa-2x text-gray-300"></i>Histórico </a></li>
+                            <li><a class="nav-link" href="des_empleados.php"><i class="fas fa-chart-line fa-2x text-gray-300"></i>Desempeño empleados <i class="fas fa-clipboard-list fa-2x text-gray-300"></i></a></li>
+                            <li><a class="nav-link" href="des_produccion.php"><i class="fas fa-chart-line fa-2x text-gray-300"></i>Desempeño producción <i class="fas fa-truck-moving fa-2x text-gray-300"></i></a></li>
+                            <li><a class="nav-link" href="desempeño_ventas.php"><i class="fas fa-chart-line fa-2x text-gray-300"></i>Desempeño ventas  <i class="fas fa-dollar-sign fa-2x text-gray-300"></i></a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <div class="text-center d-none d-md-inline"><button class="btn rounded-circle border-0" id="sidebarToggle" type="button"></button></div>
+            </div>
+        </nav>
