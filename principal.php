1277d29c9196c3c0524a353e35c35e7b63bb6fe9
Salma Cruz Sánchez <salmasanchezscs@outlook.com>
2021-12-07 14:55:12 -0600

    Principal
    Primer vista que ve el usuario al acceder al sistema.

---
+++ principal.php
@@ -0,0 +1,28 @@
+<?php
+include("templates/menu.php");
+?>
+
+<div class="d-flex flex-column" id="content-wrapper">
+    <div id="content">
+        <div class="container-fluid">
+            <br>
+
+            <!--Título-->
+            <h3 class="text-dark mb-4">Bienvenido.</h3>
+
+            <!--Imagen con filosoía-->
+            <div>
+                <div class="ritt-container"><img src="assets/img/logo.jpeg" style="width:100%">
+                    <div class="ritt-content">
+                        <strong><h2>Filosofía</h2></strong>
+                        <p>“Ser un esfuerzo constante de gente capacitada, productiva, especializada y comprometida en elaborar productos de alta calidad y a un precio razonable”.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <?php
+    include("templates/footer.php");
+    ?>
