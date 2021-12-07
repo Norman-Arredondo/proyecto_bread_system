34ee8b06c67f0546d81375ab84bb61fb0a0bf6a8
Salma Cruz Sánchez <salmasanchezscs@outlook.com>
2021-12-07 14:54:47 -0600

    Index
    Login del sistema operacional y del data warehouse.

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Login</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="assets/css/Collapsible-Sidebar.css">
+    <link rel="stylesheet" href="assets/css/Responsive-Image-with-Transparent-Text.css">
+    <link rel="stylesheet" href="assets/css/sidebar-1.css">
+    <link rel="stylesheet" href="assets/css/sidebar.css">
+    <link rel="stylesheet" href="assets/css/THANOS_fullscreen_image_with_text.css">
+    <link rel="stylesheet" href="assets/css/Transparent-Overlay-On-Image-On-Hover.css">
+</head>
+
+<body class="bg-gradient-primary">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-9 col-lg-12 col-xl-10">
+                <div class="card shadow-lg o-hidden border-0 my-5">
+                    <div class="card-body p-0">
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-flex">
+                                <div class="flex-grow-1 bg-login-image" style="background-image: url(&quot;assets/img/login.jpg&quot;);"></div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h4 class="text-dark mb-4">Bienvenido!</h4>
+                                    </div>
+                                    <form class="user">
+                                        <div class="mb-3"><input class="form-control form-control-user" type="" id="txtLog" placeholder="Usuario" name=""></div>
+                                        <div class="mb-3"><input class="form-control form-control-user" type="password" id="txtPass" placeholder="Contraseña" name=""></div>
+
+                                        <button class="btn btn-dark btn-user w-100" type="button" onClick="btnEnviar_click()">Ingresar</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/chart.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="assets/js/theme.js"></script>
+    <script src="assets/js/script.js"></script>
+</body>
+<script>
+    function btnEnviar_click() {
+        var textLogin = document.getElementById("txtLog");
+        var textPass = document.getElementById("txtPass");
+        var principal;
+
+        if (textLogin.value == "" || textPass == "") {
+            alert("Llene todos los campos")
+        } else if (textLogin.value == "JessicaM" && textPass.value == "1234") {
+            //rutina de envio
+            var principal = window.location.href = "principal.php";
+        } else {
+            alert("Usuario o contraseña incorrectos");
+        }
+
+    }
+</script>
+
+</html>
