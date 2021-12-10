b48e6f21d78111a23b4500bd7d1f64a62586422d
Salma Cruz Sánchez <salmasanchezscs@outlook.com>
2021-12-10 12:26:34 -0600

    Insert
    Se agrega un registro para probar el cambio de estatus.

--- bd/inserts.sql
+++ bd/inserts.sql
@@ -2,3 +2,7 @@ USE so_bs;
 
 -- >>> insert punto_venta <<<
 EXEC sp_Registro_ptovta 'PTO-00001', 'Matriz', 'De los Colegios', 'Los Reyes Acozac','11', 'N/A', 'Tecamac', 55755;
+EXEC sp_Registro_ptovta 'PTO-00002', 'Centro', 'De los Colegios', 'Los Reyes Acozac','15', 'N/A', 'Tecamac', 55755;
+
+-- >>> cambiar estatus punto_venta <<<
+EXEC sp_CamEst_ptovta 'PTO-00002', 0
